769c09858c44e0789dbdce469059f2ae5f099b91
Krishna Venkatramani (Rain Work GPGPassphrase) <krishna@rain.bh>
2022-09-26 13:32:44 +0400

    Added Event Detail!

--- SignalMVP/Views/Events/EventSingleCell.swift
+++ SignalMVP/Views/Events/EventSingleCell.swift
@@ -41,7 +41,8 @@ class EventSingleCell: ConfigurableCell {
 	private func setupViews() {
 		let infoStack: UIStackView = .VStack(subViews: [eventTitle, newsArticleCount, tickersView],spacing: 10)
 		let stack: UIStackView = .HStack(subViews: [imgView, infoStack], spacing: 16, alignment: .top)
-		let mainStack: UIStackView = .VStack(subViews: [stack, .divider(color: .white.withAlphaComponent(0.5)).embedInView(insets: .init(top: 10, left: 0, bottom: 0, right: 0))],
+		let divider: UIView =  .divider(color: .white.withAlphaComponent(0.5)).embedInView(insets: .zero)
+		let mainStack: UIStackView = .VStack(subViews: [stack, divider],
 											 spacing: 12)
 		contentView.addSubview(mainStack)
 		contentView.setFittingConstraints(childView: mainStack, insets: .init(vertical: 10, horizontal: 16))
@@ -57,22 +58,23 @@ class EventSingleCell: ConfigurableCell {
 	func configure(with model: EventCellModel) {
 		
 		tickersView.removeChildViews()
-		
+
+		print("(DEBUG) Configuring Cell!")
 		model.model.eventName.styled(font: .systemFont(ofSize: 15, weight: .semibold), color: .white).render(target: eventTitle)
 		eventTitle.numberOfLines = 2
-		
+
 		if let firstImgURL = model.model.news.first?.imageUrl {
 			UIImage.loadImage(url: firstImgURL, at: imgView, path: \.image)
 		}
-		
+
 		"\(model.model.news.count) News Articles".styled(font: .systemFont(ofSize: 13, weight: .regular), color: .gray).render(target: newsArticleCount)
-		
+
 		tickersView.isHidden = model.model.tickers.isEmpty
 		if !model.model.tickers.isEmpty {
 			model.model.tickers.limitTo(to: 3).forEach {
 				let label = UILabel()
 				$0.styled(font: .systemFont(ofSize: 13, weight: .regular), color: .white).render(target: label)
-				
+
 				tickersView.addArrangedSubview(label.blobify(backgroundColor: .white.withAlphaComponent(0.3),
 															 borderColor: .white,
 															 borderWidth: 1,
